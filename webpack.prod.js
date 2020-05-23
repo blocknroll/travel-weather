a66198e5f707968482cc6254a65da8efcc24265a
Fred Block <info@fredblock.com>
2020-05-23 14:38:37 -0500

    Add Client library to webpack output

--- webpack.prod.js
+++ webpack.prod.js
@@ -7,6 +7,10 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: './src/client/index.js',
+  output: {
+      libraryTarget: 'var',
+      library: 'Client'
+  },
   mode: 'production',
   devtool: 'source-map',
   module: {
