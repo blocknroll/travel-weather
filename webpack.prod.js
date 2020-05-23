f8f8efec1d95386eea763324bfb4fb613563ed33
Fred Block <info@fredblock.com>
2020-05-23 15:01:37 -0500

    Add mini-css-extract-plugin, optimize-css-assets-webpack-plugin, and terser-webpack-plugin

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,6 +4,8 @@ const path = require('path');
 const webpack = require('webpack');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
   entry: './src/client/index.js',
@@ -25,6 +27,9 @@ module.exports = {
         use: [ MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader' ]
       }
     ],
+  },
+  optimization: {
+    minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})],
   },
   plugins: [
     new HtmlWebPackPlugin({
