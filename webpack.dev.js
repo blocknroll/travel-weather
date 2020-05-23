52b00f485ea6a022db0590b7eaf24ba83b7a2033
Fred Block <info@fredblock.com>
2020-05-23 11:17:51 -0500

    Add clean-webpack-plugin

--- webpack.dev.js
+++ webpack.dev.js
@@ -3,23 +3,34 @@
 const path = require('path');
 const webpack = require('webpack');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/client/index.js',
   mode: 'development',
   module: {
     rules: [
-        {
-            test: '/\.js$/',
-            exclude: /node_modules/,
-            loader: "babel-loader"
-        }
+      {
+        test: '/\.js$/',
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      }
     ],
   },
   plugins: [
     new HtmlWebPackPlugin({
-        template: "./src/client/views/index.html",
-        filename: "./index.html",
+      template: "./src/client/views/index.html",
+      filename: "./index.html",
+    }),
+    new CleanWebpackPlugin({
+      // Simulate the removal of files: default: false
+      dry: true,
+      // Write Logs to Console, default: false
+      verbose: true,
+      // Automatically remove all unused webpack assets on rebuild: default: true
+      cleanStaleWebpackAssets: true,
+      // Do not allow removal of current webpack assets: default: true
+      protectWebpackAssets: false
     })
-]
+  ]
 };
