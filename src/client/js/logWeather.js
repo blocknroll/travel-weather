ae9b40a3b441e926e89911298d0a060e7bd9e89b
Fred Block <info@fredblock.com>
2020-05-26 16:57:34 -0500

    Add countdown function to logWeather.js. Update view in index.html

--- src/client/js/logWeather.js
+++ src/client/js/logWeather.js
@@ -16,13 +16,42 @@ function logWeather(e){
   let city = document.querySelector('#city').value;
 
 
+  // countdown timer ///////////////////////////
+  const day = document.querySelector('#day').value;
+  const month = document.querySelector('#month').value;
+  const year = document.querySelector('#year').value;
+  const date = year + '-' + month + '-' + day;
+  let tripDate = new Date(date).getTime();
+
+  const countdown = setInterval (function() {
+    // current date
+    let now = new Date().getTime();
+    // interval between now...trip date
+    let interval = tripDate - now;
+
+    let days = Math.floor( interval / (1000 * 60 * 60 * 24) );
+
+    document.querySelector('#interval').innerHTML = days + ' more days!';
+
+    if (days < 0) {
+      clearInterval(countdown);
+      document.querySelector('#interval').innerHTML = 'TODAY IS THE DAY!!!';
+    } else if (days < 1) {
+      document.querySelector('#interval').innerHTML = 'leaving tomorrow!!!';
+    } else if (days < 2 ) {
+      document.querySelector('#interval').innerHTML = days + ' more day!!!';
+    }
+  }, 1000);
+
+
+
   Client.getLatLng(GEONAMES_baseURL, city, GEONAMES_API_KEY);
 
   Client.getWeather(OPENWEATHER_baseURL, zip, OPENWEATHER_API_KEY)
   .then(function(data){
     // add data - Call Function
     Client.postData('/addData',
-            {temperature:data.main.temp, date:newDate, feelings:feelings}
+                   {temperature:data.main.temp, date:newDate, feelings:feelings}
     );
   })
   .then(function() {
